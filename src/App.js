8db5083feb90d098fe8cd6b02072d8309e74b425
Adem <adem.altan@readify.net>
2019-04-17 17:41:15 +1000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,472 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { Container, Table } from "reactstrap";
+import ReactStickies from "react-stickies";
+import Card from "react-bootstrap/Card";
+import Navigation from "./components/Navigation";
+import { Alert } from 'reactstrap';
 
 class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state = {
+     thisMonday: null,
+     nextMonday: null
+   }
+ }
+  componentDidMount() {
+    var curr = new Date();
+    var first = curr.getDate() - curr.getDay();
+    var first = first + 1;
+    var last = first + 7;
+    var thisMonday = new Date(curr.setDate(first)).toDateString();
+    var nextMonday = new Date(curr.setDate(last)).toDateString();
+    this.setState({
+      thisMonday: thisMonday,
+      nextMonday: nextMonday
+    });
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navigation />
+        <Container>
+          <Table striped>
+            <thead>
+              <tr>
+                <th>
+                  <Alert color="info">
+                    {this.state.thisMonday}
+                  </Alert>
+                </th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+                <th>
+                  <Alert color="info">
+                    {this.state.nextMonday}
+                  </Alert>
+                </th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+                <th>Sunday</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row"><Alert color="warning">Readify</Alert></th>
+                <td>
+                  <div className="each-col">
+                    <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td> </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+              </tr>
+              <tr>
+              <th scope="row"><Alert color="warning">Elsewhere</Alert></th>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td> </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card>
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+                <td>
+                  <div className="each-col">
+                  <Card className="each-card">
+                      <Card.Body>
+                        <Card.Title>Card title</Card.Title>
+                        <Card.Text>
+                          <small className="text-muted">
+                            Last updated 3 mins ago
+                          </small>
+                        </Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
       </div>
     );
   }
 }
-
 export default App;
